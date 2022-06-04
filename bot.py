705cb41c75c05f476efed371b728143eb8e4b4c9
reem_mikulsky <reempro112@gmail.com>
2022-06-04 16:20:27 +0300

    added more stuff?

--- bot.py
+++ bot.py
@@ -3,7 +3,7 @@
 def move_penguins(target_iceberg) -> None:
     """
     Move penguins from mulitple icebergs to the target iceberg
-    build a brig when there are spare pengiuens and we need this distance
+    build a brig when there are spare pengiuns and we need this distance
     """
     else
         ...
@@ -14,20 +14,20 @@ def worth_to_use_brig(source_iceberg, destination_iceberg) -> bool:
     Note: source_iceberg must be the bot's iceberg, unlike `destination_iceberg` who can be any iceberg (defense, conquer, attack)
     """
     return source_iceberg.turns_till_arrival(destination_iceberg) > 5 and 
-           get_spare_pengiuens(source_iceberg) - source_iceberg.brig_cost() >= 0
+           get_spare_pengiuns(source_iceberg) - source_iceberg.brig_cost() >= 0
 
-def get_spare_pengiuens(iceberg) -> int:
+def get_spare_pengiuns(iceberg) -> int:
     """
-    Finds the iceberg's spare pengiuens count
+    Finds the iceberg's spare pengiuns count
     1. threat to the iceberg
     2. does it need more upgrades? in realation to the other iceberg's levels
     3. does it a startigic place for defense ? -> get_stratigic_points
     4. does it a good thing to attack with? -> get_stratigic_points to enemy icebergs
     maybe remove some cases, doens't need to be complicated
     """
-    essential_pengiuens_for_iceberg = 0
+    essential_pengiuns_for_iceberg = 0
 
-    return max(iceberg.get_pengiuens() - essential_pengiuens_for_iceberg, 0)
+    return max(iceberg.get_pengiuns() - essential_pengiuens_for_iceberg, 0)
 
 
 def iceberg_points(iceberg) -> int:
@@ -55,6 +55,16 @@ def iceberg_upgrade_points(iceberg) -> int:
     """
     return (iceberg.get_penguins() - icebreg.get_upgrade_cost()) * iceberg.level() * get_stratigic_points(iceberg)
 
+def relative_importance(iceberg, get_points):
+    """
+    Finds the relative importance of an iceberg 
+    all icebergs points / iceberg points
+    """
+    return sum(icebergs)
+
+def icebergs(game):
+    return game.get_icebergs() | game.get_enemy_icebergs()
+
 def turn():
     """
     Get the best iceberg and the best move (upgrade or move) and then execute it
@@ -62,8 +72,8 @@ def turn():
     1. ratio(max()) > < = ratio(max())
     """
 
-    target_iceberg = max(game.get_icebergs(), key=iceberg_points)
-    upgrade_iceberg = max(game.get_icebergs(), key=iceberge_upgrade_points)
+    target_iceberg = max(icebergs(game), key=iceberg_points)
+    upgrade_iceberg = max(icebergs(game), key=iceberge_upgrade_points)
     
     # I can't really do this, they are not the same scoring machenism
     if where_in_move_ratio(target_iceberg) > where_in_upgrade_ratio(upgrade_iceberg):
